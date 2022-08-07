e1c197111da909a256e558dbc83da9ba5ac889e7
Rodolfo Cuevas <rcuevaspantoja@gmail.com>
2022-08-07 13:28:40 -0400

    actualziación 8-7-2022

--- src/componentes/Proyectos/Proyectos.js
+++ src/componentes/Proyectos/Proyectos.js
@@ -1,46 +1,19 @@
-import React, { useState } from 'react'
-import Box from '@mui/material/Box';
-import Typography from '@mui/material/Typography';
-import { createTheme } from '@material-ui/core/styles';
+import React from 'react'
 import './Proyectos.css'
-import logoOrizon from '../../archivos/orizon-logo-2.png';
+import ProyectosNormal from './ProyectosNormal'
+import ProyectosMobil from './ProyectosMobil'
+
 
 function Proyectos() {
 
-  const theme = createTheme({
-    typography: {
-      fontFamily: [
-        'Varela Round',
-        'sans-serif',
-      ].join(','),
-  },});
+
 
 
   return (
-    <div className='AcercaProyectos'>
+    <div className='Proyectos'>
 
-    <div className='Titulo'>
-      <Typography theme={theme} style={{ fontSize: 35, color:'#000000', textDecoration: 'none'}} > 
-        Experiencias
-      </Typography>
-    </div>
-
-
-      <Box className='AcercaProyectos-box'
-          sx={{
-              width: '100%',
-              height: '100%',
-          }}>
-
-          <div className='AcercaProyectos-pasatiempos'>
-              <Typography theme={theme} variant='h6' color="common.white"> Mayo-2022 al Presente </Typography>
-          </div>   
-          <div className='Descripcion-Empleo'>
-            <Typography theme={theme} variant='h4' color="common.white"> Operador de Automatización en Orizon S.A </Typography> <br />
-            <Typography theme={theme} variant='h7' color="common.white"> (Full-Time) </Typography>
-          </div>             
-
-      </Box>
+      <ProyectosNormal/>
+      <ProyectosMobil/>
     </div>
   )
 }
