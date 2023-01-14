9baa020b88e111ab88d1aef1fb46d9deeceed7f7
Rodolfo Cuevas <50083009+rcuevaspantoja@users.noreply.github.com>
2023-01-14 15:49:19 +0000

    14-1-23

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Navbar from './componentes/navbar/Navbar'
 import {
   Route,
   Routes,
+  Navigate
 } from "react-router-dom";
 import Acerca from './componentes/Acerca/Acerca'
 import Curriculum from './componentes/Curriculum/Curriculum'
@@ -12,34 +13,15 @@ import Proyectos from './componentes/Proyectos/Proyectos'
 function App() {
   return (    
     
-/*    <BrowserRouter>
-        <div className="App">
-          <Navbar/>
-          <Routes>
-
-            <Route path="/">
-              <div className='todo-contenido'>
-                <Acerca />
-                <Proyectos />
-                <Curriculum />
-              </div>
-            </Route>
-
-          </Routes>  
-
-        </div>       
-      </BrowserRouter> */
-
-
-
-
   <>
   
     <Navbar/>
 
     <div className="App">
       <Routes>
-        <Route path={"/"} element={<Acerca />} />
+        /*Se desplaza desde el path / al Acerca donde se encuentra la página */
+        <Route path='/' element={ <Navigate to="/Acerca" /> }/>
+        <Route path={"/Acerca"} element={<><Acerca /><Curriculum /><Proyectos /></>} />
       </Routes>
     </div>  
       
